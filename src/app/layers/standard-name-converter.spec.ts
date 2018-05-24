2563d1894c2c398cee7df7162a04e9770e6a9669
Anton Telechev <anton.teleshev@wanadoo.fr>
2018-05-24 12:37:35 +0200

    Fix tests for StandardNameConverter

--- src/app/layers/standard-name-converter.spec.ts
+++ src/app/layers/standard-name-converter.spec.ts
@@ -15,104 +15,104 @@ describe('StandardNameConverter', () => {
   };
 
   it('#toGreekLetter should return α for Alp ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Alp ZZZ'), '&alpha;');
+    assertLetterExpected(util.toGreekLetter('Alp ZZZ'), 'α');
   });
 
   it('#toGreekLetter should return β for Bet ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Bet ZZZ'), '&beta;');
+    assertLetterExpected(util.toGreekLetter('Bet ZZZ'), 'β');
   });
 
   it('#toGreekLetter should return γ for Gam ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Gam ZZZ'), '&gamma;');
+    assertLetterExpected(util.toGreekLetter('Gam ZZZ'), 'γ');
   });
 
   it('#toGreekLetter should return δ for Del ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Del ZZZ'), '&delta;');
+    assertLetterExpected(util.toGreekLetter('Del ZZZ'), 'δ');
   });
 
   it('#toGreekLetter should return ε for Eps ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Eps ZZZ'), '&epsilon;');
+    assertLetterExpected(util.toGreekLetter('Eps ZZZ'), 'ε');
   });
 
   it('#toGreekLetter should return ζ for Zet ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Zet ZZZ'), '&zeta;');
+    assertLetterExpected(util.toGreekLetter('Zet ZZZ'), 'ζ');
   });
 
   it('#toGreekLetter should return η for Eta ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Eta ZZZ'), '&eta;');
+    assertLetterExpected(util.toGreekLetter('Eta ZZZ'), 'η');
   });
 
   it('#toGreekLetter should return θ for The ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('The ZZZ'), '&theta;');
+    assertLetterExpected(util.toGreekLetter('The ZZZ'), 'θ');
   });
 
   it('#toGreekLetter should return ι for Iot ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Iot ZZZ'), '&iota;');
+    assertLetterExpected(util.toGreekLetter('Iot ZZZ'), 'ι');
   });
 
   it('#toGreekLetter should return κ for Kap ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Kap ZZZ'), '&kappa;');
+    assertLetterExpected(util.toGreekLetter('Kap ZZZ'), 'κ');
   });
 
   it('#toGreekLetter should return λ for Lam ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Lam ZZZ'), '&lambda;');
+    assertLetterExpected(util.toGreekLetter('Lam ZZZ'), 'λ');
   });
 
   it('#toGreekLetter should return μ for Mu ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Mu ZZZ'), '&mu;');
+    assertLetterExpected(util.toGreekLetter('Mu ZZZ'), 'μ');
   });
 
   it('#toGreekLetter should return ν for Nu ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Nu ZZZ'), '&nu;');
+    assertLetterExpected(util.toGreekLetter('Nu ZZZ'), 'ν');
   });
 
   it('#toGreekLetter should return ξ for Xi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Xi ZZZ'), '&xi;');
+    assertLetterExpected(util.toGreekLetter('Xi ZZZ'), 'ξ');
   });
 
   it('#toGreekLetter should return ο for Omi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Omi ZZZ'), '&omicron;');
+    assertLetterExpected(util.toGreekLetter('Omi ZZZ'), 'ο');
   });
 
   it('#toGreekLetter should return π for Pi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Pi ZZZ'), '&pi;');
+    assertLetterExpected(util.toGreekLetter('Pi ZZZ'), 'π');
   });
 
   it('#toGreekLetter should return ρ for Rho ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Rho ZZZ'), '&rho;');
+    assertLetterExpected(util.toGreekLetter('Rho ZZZ'), 'ρ');
   });
 
   it('#toGreekLetter should return σ for Sig ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Sig ZZZ'), '&sigma;');
+    assertLetterExpected(util.toGreekLetter('Sig ZZZ'), 'σ');
   });
 
   it('#toGreekLetter should return τ for Tau ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Tau ZZZ'), '&tau;');
+    assertLetterExpected(util.toGreekLetter('Tau ZZZ'), 'τ');
   });
 
   it('#toGreekLetter should return υ for Ups ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Ups ZZZ'), '&upsilon;');
+    assertLetterExpected(util.toGreekLetter('Ups ZZZ'), 'υ');
   });
 
   it('#toGreekLetter should return φ for Phi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Phi ZZZ'), '&phi;');
+    assertLetterExpected(util.toGreekLetter('Phi ZZZ'), 'φ');
   });
 
   it('#toGreekLetter should return χ for Chi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Chi ZZZ'), '&chi;');
+    assertLetterExpected(util.toGreekLetter('Chi ZZZ'), 'χ');
   });
 
   it('#toGreekLetter should return ψ for Psi ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Psi ZZZ'), '&psi;');
+    assertLetterExpected(util.toGreekLetter('Psi ZZZ'), 'ψ');
   });
 
   it('#toGreekLetter should return ω for Ome ZZZ', () => {
-    assertLetterExpected(util.toGreekLetter('Ome ZZZ'), '&omega;');
+    assertLetterExpected(util.toGreekLetter('Ome ZZZ'), 'ω');
   });
 
 
   it('#toGreekLetter should return ω for Ome', () => {
-    assertLetterExpected(util.toGreekLetter('Ome'), '&omega;');
+    assertLetterExpected(util.toGreekLetter('Ome'), 'ω');
   });
 
   it('#toGreekLetter should throw expected error for undefined arg', () => {
