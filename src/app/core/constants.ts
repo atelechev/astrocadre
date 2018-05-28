a6c6164ac3fa975c208749f7634b93f7370ae803
Anton Telechev <anton.teleshev@wanadoo.fr>
2018-05-28 12:21:32 +0200

    Add doc and cleanup in Constants

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,17 +1,33 @@
 import { Vector3 } from 'three';
 
+/**
+ * Contains constant values that might be used anywhere across the entire application.
+ */
 export class Constants {
 
+  /**
+   * The width of the main view in pixels.
+   */
   public static readonly VIEW_WIDTH: number = 700;
 
+  /**
+   * The height of the main view in pixels.
+   */
   public static readonly VIEW_HEIGHT: number = 700;
 
+  /**
+   * The radius of the 3D world sky sphere, in Three coordinate units.
+   */
   public static readonly WORLD_RADIUS: number = 2;
 
-  public static readonly STAR_MAGNITUDES: number[] = [ 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6 ];
-
+  /**
+   * The coordinate of the North pole in the 3D world.
+   */
   public static readonly NORTH = new Vector3(0, 0, Constants.WORLD_RADIUS);
 
+  /**
+   * The coordinate of the South pole in the 3D world.
+   */
   public static readonly SOUTH = new Vector3(0, 0, -Constants.WORLD_RADIUS);
 
 }
