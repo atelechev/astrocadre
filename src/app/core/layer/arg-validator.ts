a5de2e11df47a2f83cb81d5e98f41c61634d86e8
Anton Telechev <anton.teleshev@wanadoo.fr>
2018-05-28 11:41:21 +0200

    Add inline doc

--- src/app/core/layer/arg-validator.ts
+++ src/app/core/layer/arg-validator.ts
@@ -9,6 +9,13 @@
 
 export class ArgValidator {
 
+  /**
+   * Validates that the first argument is not null, not undefined, not an empty string or not a NaN.
+   * Throws an error with a respective message otherwise.
+   *
+   * @param arg the argument to validate.
+   * @param argName the name of the argument, used in the error message.
+   */
   public static ensureArgDefined(arg: any, argName: string): void {
     if (!arg && (arg === null || arg === undefined || arg === '' || isNaN(arg))) {
       this.throwErrorForInvalidArg(arg, argName);
@@ -19,6 +26,13 @@ export class ArgValidator {
     throw new Error(`${argName} arg must be defined, but was '${arg}'`);
   }
 
+  /**
+   * Validates that the string of the first argument is defined and not empty and does not contain only spaces.
+   * Throws an error with a respective message otherwise.
+   *
+   * @param arg the argument to validate.
+   * @param argName the name of the argument, used in the error message.
+   */
   public static ensureStringDefinedNotEmpty(arg: string, argName: string): void {
     if (arg === null || arg === undefined || arg.trim() === '') {
       this.throwErrorForInvalidArg(arg, argName);
--- src/app/core/layer/labelled-layer.ts
+++ src/app/core/layer/labelled-layer.ts
@@ -2,14 +2,23 @@ import { RenderableLayer } from './renderable-layer';
 import { ItemsTreeNode } from '../items-tree-node';
 import { RenderableText } from './label/renderable-text';
 
+/**
+ * Provides RenderableLayer the ability to use text labels.
+ */
 export abstract class LabelledLayer extends RenderableLayer {
 
   constructor(tree: ItemsTreeNode) {
     super(tree);
   }
 
+  /**
+   * Returns the array of HTMLElements with text contents to show for this layer.
+   */
   public abstract getTextElements(): Array<HTMLElement>;
 
+  /**
+   * Returns the Map of existing RenderableText items and their identifiers, registered for this layer.
+   */
   public abstract getRenderableLabels(): Map<string, RenderableText>;
 
 }
