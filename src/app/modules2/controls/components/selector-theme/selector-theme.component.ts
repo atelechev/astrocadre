0d46cf8550d2406b3def68cb0a8348158ce5674d
Anton Telechev <anton.teleshev@wanadoo.fr>
2021-03-07 10:53:26 +0100

    Add theme selector

---
+++ src/app/modules2/controls/components/selector-theme/selector-theme.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ThemeMeta } from 'src/app/modules2/core/models/theme-meta';
+import { EventsService } from 'src/app/modules2/core/services/events.service';
+import { ThemeService } from 'src/app/modules2/core/services/theme.service';
+
+
+@Component({
+  selector: `ac-controls-select-theme`,
+  templateUrl: './selector-theme.component.html'
+})
+export class SelectorThemeComponent {
+
+  constructor(
+    private readonly _themeService: ThemeService
+  ) {
+
+  }
+
+  public get themes(): Array<ThemeMeta> {
+    return this._themeService.availableThemes;
+  }
+
+  public themeSelected(code: string): void {
+    this._themeService.loadTheme(code);
+  }
+
+}
--- src/app/modules2/controls/controls.module.ts
+++ src/app/modules2/controls/controls.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { ControlsComponent } from 'src/app/modules2/controls/components/controls/controls.component';
+import { SelectorThemeComponent } from 'src/app/modules2/controls/components/selector-theme/selector-theme.component';
 
 @NgModule({
+  imports: [
+    BrowserModule
+  ],
   declarations: [
-    ControlsComponent
+    ControlsComponent,
+    SelectorThemeComponent
   ],
   exports: [
     ControlsComponent
