05ecc6b895de3b109e299d6e58dd23453130e5c5
Anton Telechev <anton.teleshev@wanadoo.fr>
2021-04-16 09:39:03 +0200

    Replace UI panel with sidebar

--- src/app/primeng-imports.module.ts
+++ src/app/primeng-imports.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
+import { FieldsetModule } from 'primeng/fieldset';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
 import { SelectButtonModule } from 'primeng/selectbutton';
+import { SidebarModule } from 'primeng/sidebar';
 import { SliderModule } from 'primeng/slider';
 
 
@@ -14,20 +16,24 @@ import { SliderModule } from 'primeng/slider';
     ButtonModule,
     CheckboxModule,
     DropdownModule,
+    FieldsetModule,
     InputNumberModule,
     InputTextModule,
     PanelModule,
     SelectButtonModule,
+    SidebarModule,
     SliderModule
   ],
   exports: [
     ButtonModule,
     CheckboxModule,
     DropdownModule,
+    FieldsetModule,
     InputNumberModule,
     InputTextModule,
     PanelModule,
     SelectButtonModule,
+    SidebarModule,
     SliderModule
   ]
 })
